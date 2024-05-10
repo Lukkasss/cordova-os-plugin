aec702accb1c5ab934efc4e236bff228edf0816a
Lucas Vilela <f.lucasgv@gmail.com>
2024-05-10 03:47:59 -0300

    Update OSPlugin.js

--- www/OSPlugin.js
+++ www/OSPlugin.js
@@ -1,13 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'OSPlugin', 'coolMethod', [arg0]);
+    exec(success, error, 'OSPlugin2', 'coolMethod', [arg0]);
 };
-
-let OSPlugin = {
-    showToast: (message, duration, successCallback, errorCallback) => {
-        exec(successCallback, errorCallback, 'OSPlugin', 'showToast', [message, duration]);
-    }
-};
-
-module.exports = OSPlugin;
